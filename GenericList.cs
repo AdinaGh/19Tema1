880440b4b6962b039489e3a710298b3e007e143e
AdinaGh <adina.lipsa@gmail.com>
2019-12-23 13:58:46 +0200

    gata

---
+++ GenericList.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _19Tema1
+{
+    public class GenericList<T> where T : IComparable<T>
+    {
+        private T[] _elements;
+        private int _maxAllowed;
+
+        public GenericList(int capacity = 1)
+        {
+            _elements = new T[capacity];
+            _maxAllowed = capacity;
+            Length = 0;
+        }
+
+        public int Length { get; private set; }
+
+        public List<T> Elements => _elements.ToList();
+
+        public void Add(T el)
+        {
+            AddToCount();
+            _elements[Length - 1] = el;
+        }
+
+        // Indexer
+        public T this[int index]
+        {
+            get
+            {
+                ValidateIndex(index);
+                return _elements[index];
+            }
+
+            set
+            {
+                ValidateIndex(index);
+                _elements[index] = value;
+            }
+        }
+
+        public void RemoveByIndex(int idx)
+        {
+            ValidateIndex(idx);
+            for (var i = idx; i < Length - 1; i++)
+            {
+                _elements[i] = _elements[i + 1];
+            }
+
+            Length--;
+        }
+
+        public void AddAtIndex(int idx, T el)
+        {
+            ValidateNewIndex(idx);
+            AddToCount();
+            for (var i = _elements.Length - 1; i > idx; i--)
+            {
+                _elements[i] = _elements[i - 1];
+            }
+
+            _elements[idx] = el;
+        }
+
+        public void Clear()
+        {
+            Array.Resize(ref _elements, 0);
+            Length = 0;
+        }
+
+        public int Find(T el)
+        {
+            return Array.IndexOf(_elements, el);
+        }
+
+        public T Min()
+        {
+            var min = _elements[0];
+            for (var i = 1; i < Length; i++)
+                if (_elements[i].CompareTo(min) < 0) min = _elements[i];
+            return min;
+        }
+
+        public T Max()
+        {
+            var max = _elements[0];
+            for (var i = 1; i < Length; i++)
+                if (_elements[i].CompareTo(max) > 0) max = _elements[i];
+            return max;
+        }
+
+        public override string ToString()
+        {
+            var splitter = ", ";
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < Length; i++)
+            {
+                sb.Append(_elements[i]);
+                if (i != Length - 1)
+                {
+                    sb.Append(splitter);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+
+        private void AddToCount()
+        {
+            Length++;
+            if (Length <= _elements.Length)
+            {
+                return;
+            }
+            _maxAllowed *= 2;
+            Array.Resize(ref _elements, _maxAllowed);
+        }
+
+
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= Length)
+            {
+                throw new ArgumentOutOfRangeException(OutOfRangeErrorMessage(Length - 1));
+            }
+        }
+
+        private string OutOfRangeErrorMessage(int length) => $"The index is out of range [0-{length}]";
+
+
+        private void ValidateNewIndex(int index)
+        {
+            if (index < 0 || index > Length)
+            {
+                throw new ArgumentOutOfRangeException(OutOfRangeErrorMessage(Length));
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19Tema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var abc = new GenericList<string>();
+            abc.Add("1");
+            abc.Add("2");
+            abc.Add("555");
+            abc[2] = "3";
+
+            Console.WriteLine(abc.ToString());
+            Console.WriteLine(abc[0]);
+            Console.WriteLine(abc[2]);
+            abc.RemoveByIndex(2);
+            Console.WriteLine(abc.ToString());
+
+            abc.Add("33");
+            abc.Add("44");
+            abc.Add("55");
+            abc.Add("66");
+            abc.Add("77");
+            abc.Add("88");
+
+            Console.WriteLine($"Max: {abc.Max()}");
+            Console.WriteLine($"Min: {abc.Min()}");
+            abc.AddAtIndex(0, "added");
+            Console.WriteLine(abc.ToString());
+
+            abc.AddAtIndex(9, "added2");
+            Console.WriteLine(abc.ToString());
+
+
+            Console.WriteLine($"Element found at index :{abc.Find("ddd222")}");
+            Console.WriteLine($"Element found at index :{abc.Find("88")}");
+
+            abc.Clear();
+            Console.WriteLine($"Clear: {abc.Length}");
+
+
+            TestErrors(abc);
+        }
+
+        private static void TestErrors(GenericList<string> abc)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Errors");
+            var i = 0;
+            try
+            {
+                i++;
+                abc[233] = "5";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {i}:{ex.Message}");
+            }
+
+            try
+            {
+                i++;
+                Console.WriteLine(abc[2333]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {i}:{ex.Message}");
+            }
+
+            try
+            {
+                i++;
+                abc.RemoveByIndex(3333);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {i}:{ex.Message}");
+            }
+            try
+            {
+                i++;
+                abc.RemoveByIndex(-1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {i}:{ex.Message}");
+            }
+
+            try
+            {
+                i++;
+                abc.AddAtIndex(-1, "ddd");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {i}:{ex.Message}");
+            }
+
+            try
+            {
+                i++;
+                abc.AddAtIndex(abc.Length, "nnn");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {i}:{ex.Message}");
+            }
+
+        }
+    }
+}
